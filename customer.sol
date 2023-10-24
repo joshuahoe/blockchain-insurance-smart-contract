d3df25f5027c48dd40f84a1a525907ee27ca8aa5
Joshua Hoe <53391878+joshuahoe@users.noreply.github.com>
2023-10-24 13:45:49 +0800

    add checks for purchasePolicy function

--- customer.sol
+++ customer.sol
@@ -9,8 +9,6 @@ contract Customer {
     mapping(address => uint256) public claims;
     mapping(uint256 => uint256) public customersPolicies; //policyId => policyPremium
     event Received(address, uint);
-    
-    
 
     constructor(address payable _insuranceProvider) payable {
         insuranceProviderAddress = payable(_insuranceProvider);
@@ -22,6 +20,8 @@ contract Customer {
     }
 
     function purchasePolicy(uint256 policyId) public payable {
+        bool policyPresent = Insurance(insuranceProviderAddress).checkPolicyAvailable(policyId);
+        require(policyPresent, "This policy is not available to be purchased!");
         uint256[] memory lstOfCustomerPolicies = Insurance(insuranceProviderAddress).getCustomerPolicies(customerAddress);
         if (lstOfCustomerPolicies.length != 0) {
             for (uint256 i = 0; i < lstOfCustomerPolicies.length; i++) {
@@ -39,7 +39,6 @@ contract Customer {
     //     require(insurancePolicies[msg.sender] > 0, "must have a valid policy to file a claim");
     //     require(amount > 0, "Claim amount  must be greater than 0.");
     //     claims[msg.sender] += amount;
-
 
     // }   
 
