f06b503abda4040879466b7f1792a72efdc71bb1
Joshua Hoe <53391878+joshuahoe@users.noreply.github.com>
2023-10-26 01:40:27 +0800

    Update customer.sol

--- customer.sol
+++ customer.sol
@@ -4,20 +4,25 @@ pragma solidity ^0.8.0;
 import "./insurance.sol"; 
 
 contract Customer {
-    address payable insuranceProviderAddress;
-    address payable customerAddress;
-    mapping(uint256 => uint256) public customersPolicies; //policyId => policyPremium
-    event Received(address, uint);
+    address payable insuranceProviderAddress; // address of insurance provider that the customer is interacting with
+    address payable customerAddress; // address of customer's own contract
+    event Received(address, uint); // an event to be emitted when any funds is received by this contract
 
+    // customer contract's constructor, takes in insurance provider's contract address
     constructor(address payable _insuranceProvider) payable {
         insuranceProviderAddress = payable(_insuranceProvider);
         customerAddress = payable(address(this));
     }
 
-     receive() external payable {
+    // receive() function to allow funds to be received
+    receive() external payable {
         emit Received(msg.sender, msg.value);
     }
 
+    // function for customer to request to purchase a policy
+    // checks if the policy is available to be purchased, check that the customer has not already purchased this policy
+    // checks that customer has enough funds to purchase this policy
+    // checks if customer is onboarded first
     function requestToPurchasePolicy(uint256 policyId) public payable {
         bool policyPresent = Insurance(insuranceProviderAddress).checkPolicyAvailable(policyId);
         require(policyPresent, "This policy is not available to be purchased!");
@@ -33,23 +38,29 @@ contract Customer {
         insuranceProviderAddress.transfer(premium);
     }
 
+    // function for customer to onboard with insurance provider
     function requestOnboard() public {
         Insurance(insuranceProviderAddress).approveCustomerOnboard();
     }
 
+    // function for customer to view all policyId of policies that are available to be purchased
     function viewAvailablePolicies() public view returns (uint256[] memory) {
         return Insurance(insuranceProviderAddress).getAllAvailablePoliciesId();
     }
 
+    // function to return the policy details given a policyId
     function viewPolicyDetails(uint256 policyId) public view returns (Insurance.availablePolicy memory) {
         return Insurance(insuranceProviderAddress).getAvailablePolicyById(policyId); 
     }
 
+    // function for customers to view its outstanding claims
     function viewOutstandingClaims(address policyHolder) public view returns (uint256) {
         return Insurance(insuranceProviderAddress).getOutstandingClaims(policyHolder);
     }
 
-
+    // function to file a claim from insurance provider
+    // checks that customer has ongoing policy and that customer is claiming from a policy he/she has purchased
+    // checks that claim amount does not exceed policy's payout amount
     function fileClaim(uint256 amount, uint256 policyId) public {
         bool present = false;
         uint256[] memory lstOfCustomerPolicies = Insurance(insuranceProviderAddress).getCustomerPolicies(customerAddress);
@@ -65,6 +76,8 @@ contract Customer {
         Insurance(insuranceProviderAddress).addToOutstandingClaims(amount, customerAddress);
     }   
 
+    // function for customer to request claim approval after filing for a claim
+    // checks that the clam amount does not exceed the outstanding claims and is greater than 0
     function requestClaimApproval(uint256 amount) public {
         uint256 outstandingClaims = Insurance(insuranceProviderAddress).getOutstandingClaims(customerAddress);
         require(amount > 0, "Claim amount must be more than 0");
